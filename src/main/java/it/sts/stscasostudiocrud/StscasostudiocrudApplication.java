144014fed79ad64ee814ce8256cad9897e83167b
Brivio Matteo <brivio.matteo01@gmail.com>
2025-10-24 03:31:13 +0200

    added model mapper

--- src/main/java/it/sts/stscasostudiocrud/StscasostudiocrudApplication.java
+++ src/main/java/it/sts/stscasostudiocrud/StscasostudiocrudApplication.java
@@ -1,13 +1,21 @@
 package it.sts.stscasostudiocrud;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StscasostudiocrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StscasostudiocrudApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper istanceModelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		return mapper;
 	}
 
 }
