62e706d3d548829abb7e7878a66b94dbb97e34b0
Brivio Matteo <brivio.matteo01@gmail.com>
2025-10-24 02:38:53 +0200

    inizio progetto

---
+++ src/main/java/it/sts/stscasostudiocrud/StscasostudiocrudApplication.java
@@ -0,0 +1,13 @@
+package it.sts.stscasostudiocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StscasostudiocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StscasostudiocrudApplication.class, args);
+	}
+
+}
